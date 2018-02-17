c0fb0b5c9d3e45338ac8f715825f7b8d8e63c1ed
Vlad_Hmara <hmara_vlad@mail.ru>
2018-02-17 19:15:19 +0200

    Add class sorterContainer

--- TRPZ-1-2018-IA-61-08/Program.cs
+++ TRPZ-1-2018-IA-61-08/Program.cs
@@ -10,6 +10,12 @@ namespace TRPZ_1_2018_IA_61_08
     {
         static void Main(string[] args)
         {
+            int[] arr = {4,2,1 };
+            SortetContainer.Sort(ref arr);
+            Console.WriteLine();
+            foreach(int i in arr)
+                Console.WriteLine(i);
+            Console.ReadKey();
         }
     }
 }
---
+++ TRPZ-1-2018-IA-61-08/SortetContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPZ_1_2018_IA_61_08
+{
+    public class SortetContainer
+    {
+        static void Swap(ref int el1, ref int el2)
+        {
+            int tmp = el2;
+            el2 = el1;
+            el1 = tmp;
+        }
+
+        public enum SortName
+        {
+            MergeSort
+        }
+
+        static public void Sort(ref int[] arr, SortName sortName = SortName.MergeSort)
+        {
+            //switch (sortName)
+            //    case
+        }
+    }
+}
