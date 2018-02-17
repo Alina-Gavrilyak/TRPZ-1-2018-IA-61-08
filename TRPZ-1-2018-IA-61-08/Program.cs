9cdaf760594234c55aaa17cd5c7fbebad271c6a3
Alina-Gavrilyak <alina.gavrilyak@gmail.com>
2018-02-17 18:13:42 +0200

    Добавьте файлы проекта.

---
+++ TRPZ-1-2018-IA-61-08/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPZ_1_2018_IA_61_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
